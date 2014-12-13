462384fef874bc263458654a165d3058f8955e6c
damr- <iweixl@hotmail.de>
2014-12-13 10:47:51 +0100

    Basic Interfaces added

---
+++ Assets/Source/TheDarkNight/Movement/IMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Source/TheDarkNight/Picking/IPickingUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IPickingUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
