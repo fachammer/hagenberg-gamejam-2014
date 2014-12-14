d786bc87d36928a27be40a4ee27cb6f638026565
damr- <iweixl@hotmail.de>
2014-12-14 20:19:38 +0100

    Batterias

--- Assets/Source/TheDarkNight/Flashlight/Battery.cs
+++ Assets/Source/TheDarkNight/Flashlight/Battery.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 using TheDarkNight.Picking;
+using TheDarkNight.Extensions;
 
 namespace TheDarkNight.FlashLight {
     public class Battery : Pickable, IBattery {
         [SerializeField]
         private float runTimeSeconds = 5f;
-        
+
+        private void Start() {
+            //transform.GetComponentsInChildren<Transform>().Where(t => t.gameObject.name != this.gameObject.name).Do(t => t.transform.rotation = Quaternion.identity);
+        }
+
         public float GetRemainingTime() {
             return runTimeSeconds;
         }
